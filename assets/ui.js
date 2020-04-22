ced5e373f82e9de5ced3e2abee06a57dddfb1f93
Timothy Brink <contact@timothybrink.dev>
2020-04-22 17:46:18 -0400

    update ui

--- assets/ui.js
+++ assets/ui.js
@@ -7,15 +7,11 @@ ui.init = function () {
     ui.toastElement = yadl.create('.toast').attach()
     ui.disabler = yadl.create('.disabler').attach()
     ui.initAccelerators()
-    ui.textEditor.init()
 
-    yadl.select('#start').listen('click', function (e) {
-      e.preventDefault()
+    // Add initial slot
+    ui.textEditor.addSlot()
 
-      startPresentation(ui.textEditor.slides)
-
-      this.remove()
-    })
+    yadl.select('#add-button').listen('click', () => ui.textEditor.addSlot())
   })
 }
 
@@ -53,26 +49,128 @@ ui.enable = function () {
     ui.disabler.removeClass('visible')
 }
 
+
+
 ui.textEditor = {}
 
-ui.textEditor.slides = []
-ui.textEditor.addSlide = function (content) {
-  let index = ui.textEditor.slides.push(content) - 1
-  yadl.create('div')
-    .attach(yadl.select('#slides'))
-    .append(yadl.create('span').text(content))
+ui.textEditor.slots = []
+ui.textEditor.newSlot = function () {
+  let textSpan = yadl.create('span')
+    .text('Enter text here...')
+    .set('contentEditable', 'true')
+    .listen('focus', function () {
+      if (this.parentElement.classList.contains('empty'))
+        ui.selectText(this)
+    })
+    .listen('input', function (e) {
+      this.parentElement.classList.remove('empty')
+
+      if (e.inputType == 'insertParagraph') {
+        // Add new slot after this one and focus
+        ui.textEditor.addSlot(this.parentElement)
+        this.parentElement.nextSibling.children[1].focus()
+        // remove any elements that were added by the insertParagraph
+        for (let i = 0; i < this.children.length; i) {
+          this.children[i].remove()
+        }
+      }
+    })
+  return yadl.create('li')
+    .append(yadl.create('button')
+      .text('x')
+      .setClass('delete')
+      .listen('click', function () {
+        this.parentElement.remove()
+      }))
+    .append(textSpan)
+    .append(yadl.create('button')
+      .text('Toggle')
+      .listen('click', ui.textEditor.toggleHandler(textSpan)))
     .append(yadl.create('button')
       .text('Show')
-      .listen('click', () => {if (presStarted) goto(index)}))
+      .listen('click', ui.textEditor.showHandler(textSpan)))
 }
 
-ui.textEditor.init = function () {
-  ui.textEditor.form = document.forms.textInput
-  ui.textEditor.form.addEventListener('submit', function (e) {
-    e.preventDefault()
+/**
+ * Add a new slot to the list of slots.
+ * If node is provided, it is an HTML (not yadl) node after which
+ * to insert the new node. Otherwise, the new node
+ * will be added at the end.
+ */
+ui.textEditor.addSlot = function (node) {
+  let slot = ui.textEditor.newSlot()
+  if (node) {
+    node.parentElement.insertBefore(slot._element, node.nextSibling)
+  } else {
+    yadl.select('#slots').append(slot)
+  }
+  slot.setClass('empty')
+}
 
-    ui.textEditor.addSlide(this.elements[0].value)
+ui.textEditor.toggleHandler = function (textElement) {
+  return function (e) {
+    let text = textElement.get('textContent')
+    presenter.toggle(text)
+    if (presenter.current == text) {
+      ui.textEditor.setAllHide()
+      ui.textEditor.disableShows()
+    } else {
+      ui.textEditor.setAllToggle()
+      ui.textEditor.enableShows()
+    }
+  }
+}
 
-    this.elements[0].value = ''
+ui.textEditor.showHandler = function (textElement) {
+  return function (e) {
+    let text = textElement.get('textContent')
+    let length = Number(yadl.select('#show-length').get('value'))
+    presenter.showFor(text, length)
+    ui.textEditor.disableShows()
+    ui.textEditor.setAllHide()
+
+    setTimeout(() => {
+      ui.textEditor.enableShows()
+      ui.textEditor.setAllToggle()
+    }, length * 1000)
+  }
+}
+
+// Some methods for enabling/disabling/updating buttons en masse
+ui.textEditor.disableShows = function () {
+  yadl.select('#slots').children.forEach(li => {
+    li.children[3].set('disabled', true)
   })
+}
+ui.textEditor.enableShows = function () {
+  yadl.select('#slots').children.forEach(li => {
+    li.children[3].set('disabled', false)
+  })
+}
+
+ui.textEditor.setAllHide = function () {
+  yadl.select('#slots').children.forEach(li => {
+    li.children[2].text('Hide')
+  })
+}
+ui.textEditor.setAllToggle = function () {
+  yadl.select('#slots').children.forEach(li => {
+    li.children[2].text('Toggle')
+  })
+}
+
+ui.selectText = function (node) {
+  if (document.body.createTextRange) {
+    const range = document.body.createTextRange()
+    range.moveToElementText(node)
+    range.select()
+  } else if (window.getSelection) {
+    const selection = window.getSelection()
+    const range = document.createRange()
+    range.selectNodeContents(node)
+    selection.removeAllRanges()
+    selection.addRange(range)
+  } else {
+    console.warn("Could not select text in node: Unsupported browser.");
+  }
 }
